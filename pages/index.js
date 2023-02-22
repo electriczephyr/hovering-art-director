6ff195fd6a3698fe7a2237ccc18f0a910ae668f9
Jayne Vidheecharoen <jaynev@gmail.com>
2023-02-22 00:39:28 -0800

    Update title

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
-export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
-export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
+export const appName = "Hovering Art Director";
+export const appSubtitle = "Describe your vision and I'll see what I can do";
+export const appMetaDescription = "Describe your vision and I'll see what I can do";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
